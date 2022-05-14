4c2fea390629d947f5f289290e4afef4c50f9b6a
t0mezz <68075129+t0mezz@users.noreply.github.com>
2022-05-14 10:45:58 +0200

    Add files via upload

--- Website/script.js
+++ Website/script.js
@@ -1,18 +1,28 @@
 window.addEventListener('scroll', function(){
     let img = document.getElementById("background-img");
-    let head = document.getElementById("head");
     let border = document.getElementById("main-head");
+    let content = document.getElementsByClassName("head-content");
+    let logo = document.getElementById("logo");
 
-    img.style = 'object-position: 0px '+(window.scrollY-70)*1.05+'px;'
-    img.style = 'filter: blur('+(window.scrollY*.1)+'px);'
-    head.style = 'margin: '+(window.scrollY+20)+'px 20px;'
+    img.style = 'object-position: 0px '+(window.scrollY-95)*1.05+'px; filter: blur('+(window.scrollY*.03)+'px);'
+    border.style = 'margin: '+(window.scrollY)+'px 0px;'
 
     if(window.scrollY >= 200)
     {
+        logo.setAttribute('src', 'assets/logo/logo.svg');
+        for(let i = 0; i < content.length; i++)
+        {
+            content[i].style = 'color: black;'
+        }
         border.classList.add("main-head-border");
     }
     else if(window.scrollY < 200)
     {
+        logo.setAttribute('src', 'assets/logo/logoLight.svg');
+        for(let i = 0; i < content.length; i++)
+        {
+            content[i].style = 'color: #E4E4E4;'
+        }
         border.classList.remove("main-head-border");
     }
 });
