c3fd98435ecd42b3b82eb7c31f9115238cfd1a4c
t0mezz <acc2tom@gmail.com>
2022-06-04 17:09:14 +0200

    deleted branch

--- Website/script.js
+++ Website/script.js
@@ -1,3 +1,14 @@
+function enterHall()
+{
+    let button=document.getElementById("button-enter");
+    let img=document.getElementById("img-banner-enter");
+    button.style = "transform: scale(100%);"
+    setTimeout(function() {button.style = "transform: scale(102%);"}, 100)
+    img.classList.add("img-banner-enter-zoom");
+    img.style = "opacity: 75%;"
+    setTimeout(function() {open("https://www.youtube.com/watch?v=dQw4w9WgXcQ")}, 500 );
+}
+
 window.addEventListener('scroll', function(){
     let value = window.pageYOffset;
 
@@ -6,12 +17,36 @@ window.addEventListener('scroll', function(){
     let border = document.getElementById("main-border");
     let content = document.getElementsByClassName("head-content");
     let logo = document.getElementById("logo");
+    let imgRetro = document.getElementById("img-retro");
+    let imgHighscore = document.getElementById("img-highscore");
+    let imgUnity = document.getElementById("img-unity");
 
-    console.log(value);
+    if(value <= 400)
+    {
+        console.log(400-Math.sin(value*(Math.PI/2/400))*400);
+    }
 
+    // parallax stuff
     img.style = 'object-position: 0px '+(value-332.5)*1.05+'px;'
     imgBlured.style = 'opacity: '+(value/300)+'; object-position: 0px '+(value-332.5)*1.05+'px;'
 
+    // table infos
+    if(value-600 <= 400)
+    {
+        imgRetro.style = 'transform: translateX('+(400-Math.sin((value-600)*(Math.PI/2/400))*400)+'%);'
+    }
+
+    if(value-800 <= 400)
+    {
+        imgHighscore.style = 'transform: translateX('+-(400-Math.sin((value-800)*(Math.PI/2/400))*400)+'%); height: 213px;'
+    }
+
+    if(value-1000 <= 400)
+    {
+        imgUnity.style = 'transform: translateX('+(400-Math.sin((value-1000)*(Math.PI/2/400))*400)+'%);'
+    }
+
+    // upper banner opacity
     if(window.scrollY >= 200)
     {
         logo.setAttribute('src', 'assets/logo/logo.svg');
@@ -31,3 +66,7 @@ window.addEventListener('scroll', function(){
         border.classList.remove("main-head-border");
     }
 });
+
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
